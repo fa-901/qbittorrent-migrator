8e60828acc8e6d263b24304c577e522f35d89fcc
Farhan Alam <farhan_w901@outlook.com>
2025-08-06 13:32:03 +0600

    feat: add bencode decoding for qBittorrent fastresume files

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import fs from 'fs';
 import path from 'path';
 import os from 'os';
 import { execSync } from 'child_process';
+import bencode from 'bencode';
 
 // 🔧 Hardcoded path to Windows qBittorrent fastresume files
-const WINDOWS_QBIT_FASTRESUME_DIR =
+const WINDOWS_QBIT_DIR =
     '/media/farhan/SSD-OS-10/Users/metal/AppData/Local/qBittorrent/BT_backup';
 // 🔧 Hardcoded base path where Linux files should be copied to
 const LINUX_DOWNLOADS_DIR = path.join(
@@ -22,14 +23,36 @@ const isQBitRunning = (): boolean => {
     }
 };
 
-if (isQBitRunning()) {
-    console.error(
-        `❌ Qbittorrent is running. Close it before running the migration.`,
-    );
+// if (isQBitRunning()) {
+//     console.error(
+//         `❌ Qbittorrent is running. Close it before running the migration.`,
+//     );
+//     process.exit(1);
+// }
+
+if (!fs.existsSync(WINDOWS_QBIT_DIR)) {
+    console.error(`❌ Directory not found: ${WINDOWS_QBIT_DIR}`);
     process.exit(1);
 }
 
-if (!fs.existsSync(WINDOWS_QBIT_FASTRESUME_DIR)) {
-    console.error(`❌ Directory not found: ${WINDOWS_QBIT_FASTRESUME_DIR}`);
+const files = await fs.promises.readdir(WINDOWS_QBIT_DIR);
+const fastResumeFiles = files.filter((f) => f.endsWith('.fastresume'));
+
+if (files.length < 1) {
+    console.log('No torrents found.');
     process.exit(1);
 }
+
+console.log(`📄 Found ${fastResumeFiles.length} torrents to migrate.`);
+
+// read the file content
+fastResumeFiles.forEach(async (file) => {
+    const filePath = path.join(WINDOWS_QBIT_DIR, file);
+    const fileContent = await fs.promises.readFile(filePath);
+    try {
+        const decoded = bencode.decode(fileContent, 'utf-8');
+        console.log(`✔ File: ${file} \n  - Save Path: ${decoded.save_path}\n`);
+    } catch (error) {
+        console.error('❌ Error decoding fastresume file:', error);
+    }
+});
